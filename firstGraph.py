0e5cff644f568d0885ea73a4d807a41f63e9a825
ReeceDudley <reecedudley24@gmail.com>
2025-12-07 14:11:44 +0000

    Add first Python graph project

---
+++ firstGraph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+# example data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+plt.plot(x, y)
+plt.title("My First Python Graph")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
